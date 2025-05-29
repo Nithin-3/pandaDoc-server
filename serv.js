7517d69f6411fb40fb50d63ae4a1f3a36a67290c
nitMux <nithin3dev@gmail.com>
2025-05-29 07:36:41 +0000

    onlineCk

--- serv.js
+++ serv.js
@@ -1,23 +1,74 @@
-const io = require('socket.io')(3030, {
-  cors: {
-    origin: "*", // The URL of your frontend
-  },
+const https = require('http');
+const exp = require('express');
+const mult = require('multer');
+const {Server} = require('socket.io')
+const rest = exp();
+const server = https.createServer(rest);
+const io = new Server (server, {
+    cors: {
+        origin: "*",
+    },
 }); 
-const online = {}
+const fls = mult({ dest:'sndFls/' })
+const online = new Map();
+const onSock = new Map();
+rest.get('/:uid',(rq,rs)=>{
+    rs.send(online.get(rq.params.uid))
+})
+rest.post('/',fls.array('array',10),(rq,rs)=>{
+    console.log(req.files);
+    rs.send(req.files.length);
+})
 io.on('connection', (socket) => {
-  socket.on('set', (roomName) => {
-    console.log(`${socket.id} is joining room: ${roomName}`);
-    socket.join(roomName);
-    online[`${roomName}`] = true;
-  });
+    socket.on('set', (roomName) => {
+        console.log(`${socket.id} is joining room: ${roomName}`);
+        socket.join(roomName);
+        onSock.set(socket.id,roomName);
+        online.set(roomName,socket.id);
+    });
 
-  socket.on('chat', (roomName, message) => {
-    console.log(message);
-    io.to(roomName).emit("msg", message);
-  });
+    socket.on('chat', (roomName, message) => {
+        console.log(message);
+        io.to(roomName).emit("msg", message);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected: ' + socket);
-  });
+    socket.on('disconnect', () => {
+        const room = onSock.get(socket.id)
+        if(room){
+            online.delete(room)
+            onSock.delete(socket.id)
+        }else{
+            const data = "unknown user disconnected"
+            const options = {
+                hostname: 'ntfy.sh',
+                path: '/eno',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': data.length
+                }
+            };
+
+            const req = https.request(options, res => {
+                let body = '';
+                res.on('data', chunk => {
+                    body += chunk;
+                });
+                res.on('end', () => {
+                    console.log('Response:', body);
+                });
+            });
+
+            req.on('error', error => {
+                console.error('Error:', error);
+            });
+
+            req.write(data);
+            req.end();
+
+
+        }
+        console.log('User disconnected: ' + socket);
+    });
 });
-
+server.listen(3030)
